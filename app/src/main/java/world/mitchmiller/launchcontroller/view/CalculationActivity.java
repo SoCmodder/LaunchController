7b4570bbd915e7f956b125b0ef31aacb9d3bb43e
SoCmodder <socmodder@gmail.com>
2017-12-24 12:53:46 -0500

    add calc activity

---
+++ app/src/main/java/world/mitchmiller/launchcontroller/view/CalculationActivity.java
@@ -0,0 +1,20 @@
+package world.mitchmiller.launchcontroller.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import world.mitchmiller.launchcontroller.R;
+
+/**
+ * Created by Mitch Miller on 12/24/2017.
+ */
+
+public class CalculationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculation);
+    }
+}
